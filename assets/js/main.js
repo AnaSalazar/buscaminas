9025d18b31a8770879e8822e6fbda4a9c8f8fb9f
AnaSalazar <anastephany.salazarguzman@gmail.com>
2017-04-04 14:22:24 -0500

    agregado de funciones y un archivo css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,40 @@
-var casillaBomba = document.getElementsByClassName("bomba");
+//saco la longitud del arreglo que me regresa el ClassName
+var longitudBombas = document.getElementsByClassName("bomba").length;
+var longitudNumeros = document.getElementsByClassName("numero").length;
+var longitudColores = document.getElementsByClassName("color").length;
 
-var casillaBomba1 = document.getElementById("cb1-1");
-var casillaBomba2 = document.getElementById("cb2-3");
-var casillaBomba3 = document.getElementById("cb2-4");
-var casillaBomba4 = document.getElementById("cb3-1");
-var casillaBomba5 = document.getElementById("cb4-2");
+//creo variables de arreglos vacios para guardar los elementos que me regresa el ClassName
+var bombas = [];
+var numeros = [];
+var colores = [];
 
-casillaBomba1.addEventListener("click", bombaEnAccion);
-casillaBomba2.addEventListener("click", bombaEnAccion);
-casillaBomba3.addEventListener("click", bombaEnAccion);
-casillaBomba4.addEventListener("click", bombaEnAccion);
-casillaBomba5.addEventListener("click", bombaEnAccion);
+//guardo los elementos y le agrego el evento
+for(var i = 0; i < longitudBombas; i++){
+    bombas[i] = document.getElementsByClassName("bomba")[i];
+    bombas[i].addEventListener("click", bombaEnAccion);
+}
+
+for(var i = 0; i < longitudNumeros; i++){
+    numeros[i] = document.getElementsByClassName("numero")[i];
+    numeros[i].addEventListener("click", numerosEnAccion);
+}
+
+for(var i = 0; i < longitudColores; i++){
+    colores[i] = document.getElementsByClassName("color")[i];
+    colores[i].addEventListener("click", colorEnAccion);
+}
+
+//funcion para cada clase
+function numerosEnAccion() {
+  this.style.color = "red";
+  this.innerText = "¡1!";
+}
 
 function bombaEnAccion() {
-  this.style.background = "purple" ;
+  this.style.color = "red";
+  this.innerText = "¡¡¡BOMBA!!!";
+}
+
+function colorEnAccion() {
+  this.style.background = "purple";
 }
